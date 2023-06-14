163072ee0148b4dc2e843c56e1eaa616d039b7c6
Anton Kiselevich <kiselevichanton@gmail.com>
2023-06-14 18:44:28 +0300

    Implement search and filtering features

--- app/src/main/java/com/example/littlelemon/ui/home/Home.kt
+++ app/src/main/java/com/example/littlelemon/ui/home/Home.kt
@@ -3,14 +3,17 @@ package com.example.littlelemon.ui.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.SearchBar
@@ -19,7 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -30,10 +35,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.littlelemon.R
@@ -44,75 +47,115 @@ import com.example.littlelemon.ui.navigation.Profile
 import com.example.littlelemon.ui.theme.light_onPrimary
 import com.example.littlelemon.ui.theme.light_primary
 import com.example.littlelemon.ui.theme.light_primaryContainer
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(navController: NavHostController) {
     val database = (LocalContext.current as MainActivity).database
     val cachedMenuItems by database.menuDao().getMenuItems().observeAsState(emptyList())
-
-    Column {
-        TopAppBar(isProfileIconVisible = true) { navController.navigate(Profile.route) }
-        Hero()
-        MenuItemList(items = cachedMenuItems)
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Hero() {
     var searchInput by rememberSaveable { mutableStateOf("") }
     var active by rememberSaveable { mutableStateOf(false) }
 
-    Surface(color = light_primary, modifier = Modifier.fillMaxWidth()) {
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = stringResource(R.string.hero_title),
-                style = typography.headlineLarge,
-                color = light_primaryContainer
-            )
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Column(modifier = Modifier.weight(1F)) {
-                    Text(
-                        text = stringResource(R.string.hero_city),
-                        style = typography.headlineSmall,
-                        color = light_onPrimary
-                    )
-                    Text(
-                        text = stringResource(R.string.hero_description),
-                        style = typography.bodyMedium,
-                        color = light_onPrimary,
-                        modifier = Modifier.padding(top = 16.dp, end = 8.dp)
+    // Get unique values from items category in database
+    val filters = cachedMenuItems.map { it.category }.distinct()
+    val selectedFilters = remember { mutableStateListOf<String>() }
+    var isSelected by rememberSaveable { mutableStateOf(false) }
+
+
+    Column {
+        TopAppBar(isProfileIconVisible = true) { navController.navigate(Profile.route) }
+        Surface(color = light_primary, modifier = Modifier.fillMaxWidth()) {
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = stringResource(R.string.hero_title),
+                    style = typography.headlineLarge,
+                    color = light_primaryContainer
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Column(modifier = Modifier.weight(1F)) {
+                        Text(
+                            text = stringResource(R.string.hero_city),
+                            style = typography.headlineSmall,
+                            color = light_onPrimary
+                        )
+                        Text(
+                            text = stringResource(R.string.hero_description),
+                            style = typography.bodyMedium,
+                            color = light_onPrimary,
+                            modifier = Modifier.padding(top = 16.dp, end = 8.dp)
+                        )
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.hero_logo),
+                        contentDescription = "Hero Logo",
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
                     )
                 }
-                Image(
-                    painter = painterResource(id = R.drawable.hero_logo),
-                    contentDescription = "Hero Logo",
+                SearchBar(
                     modifier = Modifier
-                        .size(120.dp)
-                        .clip(RoundedCornerShape(8.dp)),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            SearchBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp),
-                query = searchInput,
-                onQueryChange = { searchInput = it },
-                onSearch = { active = false },
-                active = active,
-                onActiveChange = { },
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = "Search icon"
-                    )
-                },
-                placeholder = { Text(text = stringResource(R.string.hero_search_placeholder)) }
-            ) {
-
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    query = searchInput,
+                    onQueryChange = { searchInput = it },
+                    onSearch = { active = false },
+                    active = active,
+                    onActiveChange = { },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "Search icon"
+                        )
+                    },
+                    placeholder = { Text(text = stringResource(R.string.hero_search_placeholder)) }
+                ) {  }
             }
         }
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            items(filters) { filterItem ->
+                isSelected = selectedFilters.contains(filterItem)
+                Spacer(modifier = Modifier.padding(4.dp))
+                FilterChip(
+                    selected = isSelected,
+                    onClick = {
+                        if (selectedFilters.contains(filterItem)) {
+                            selectedFilters.remove(filterItem)
+                        } else {
+                            selectedFilters.add(filterItem)
+                        }
+                    },
+                    label = {
+                        Text(text = filterItem.replaceFirstChar { letter ->
+                            if (letter.isLowerCase()) letter.titlecase(
+                                Locale.ROOT
+                            ) else letter.toString()
+                        })
+                    }
+                )
+            }
+        }
+
+        // Search filtering
+        var filteredMenuItems = if (searchInput.isNotEmpty()) {
+            cachedMenuItems.filter { searchInput in it.title }
+        } else {
+            cachedMenuItems
+        }
+        // Chip filtering
+        filteredMenuItems = if (selectedFilters.isNotEmpty()) {
+            filteredMenuItems.filter { it.category in selectedFilters }
+        } else {
+            filteredMenuItems
+        }
+
+        MenuItemList(items = filteredMenuItems)
     }
 }
 
@@ -122,7 +165,7 @@ fun MenuItemList(items: List<MenuEntity>) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp, 8.dp)
     ) {
         items(
             items = items,
@@ -162,10 +205,4 @@ fun MenuItemList(items: List<MenuEntity>) {
             }
         )
     }
-}
-
-@Preview
-@Composable
-fun HomePreview() {
-    Home(navController = rememberNavController())
 }
